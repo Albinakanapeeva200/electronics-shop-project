326e8c067041379171a2ff34913f471389898ba5
20Alba <albinakanapeeva20@icloud.com>
2023-10-04 00:22:55 +0400

    add

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    """Тест на общую стоимость конкретного товара в магазине"""
+    item1 = Item("toy", 500.0, 3)
+    assert item1.calculate_total_price() == 1500.0
+
+
+def test_apply_discount():
+    """Тест на установленную скидку для конкретного товара"""
+    item1 = Item("toy", 500.0, 3)
+    item1.pay_rate = 0.6
+    item1.apply_discount()
+    assert item1.price == 300
